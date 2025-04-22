f974a75c2f7c6eec255f263b3f7bc40a3595ac0b
Lucas Patrick Ramos de Oliveira <lpro@palmares.ifpe.edu.br>
2025-04-22 10:52:43 -0300

    First application in the Kotlin - DEV Mobile

---
+++ MyApplicationMobile/app/src/main/java/com/lpro/myapplicationmobile/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.lpro.myapplicationmobile
+
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import kotlin.random.Random
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+    fun onClick(view: View) {
+        val textResult = findViewById<TextView>(R.id.textResult)
+        val porcentagem = Random.nextInt(1,100)
+
+        textResult.setText(porcentagem.toString())
+    }
+
+    fun msg() {
+        val msg = Mensagem()
+        msg.main()
+    }
+
+
+}
